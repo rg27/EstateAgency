1bd3fcc33a19983dc8d130e2e9f97771b3f26a46
rg27 <gualvezreymar@gmail.com>
2021-08-23 14:58:26 +0800

    Refactoring codes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: EstateAgency - v2.2.1
-* Template URL: https://bootstrapmade.com/real-estate-agency-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function($) {
   "use strict";
 
